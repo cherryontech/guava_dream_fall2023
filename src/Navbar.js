8c4a8e06bb7a8ea84c6a9cf9f0ed7dca86e9e236
Gabby Recny <gjtrecny@gmail.com>
2023-10-16 15:53:12 -0600

    chore: Move navbar component to src directory

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+      </ul>
+    </nav>
+  );
+}
