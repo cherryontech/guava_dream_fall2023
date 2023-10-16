16f9d98019e1887512f4563862e52ed11d8a781f
Gabby Recny <gjtrecny@gmail.com>
2023-10-16 15:59:49 -0600

    chore: Move header component into main src directory and rename file

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'React';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Equivate</h1>
+      <h3>Your Workplace Equity Navigator</h3>
+    </header>
+  )
+}
+
+export default Header;
