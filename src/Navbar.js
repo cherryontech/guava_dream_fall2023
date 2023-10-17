1c8af2de937c174e2e607b0e826ea080657ddd2a
Gabby Recny <gjtrecny@gmail.com>
2023-10-17 09:07:20 -0600

    chore: Adjust indentation to pass esLint check

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-  <nav>
-    <ul>
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
-      <li>Item 4</li>
-      <li>Item 5</li>
-    </ul>
-  </nav>
+    <nav>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ul>
+    </nav>
   );
 };
 
