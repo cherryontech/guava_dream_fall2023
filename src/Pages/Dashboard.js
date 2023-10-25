fc53d0eea48884d0d1f1374a51d4b26705e9643a
meganbyrd22 <meganbyrd22@gmail.com>
2023-10-25 12:46:40 -0500

    chore: Add div for Dashboard chore: Add placeholders for conntenrs of Dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,6 +1,18 @@
-const Dashboard= () => {
+const Dashboard = () => {
+    return (
+        //Add Nav component
+        //Add SideMenu component
+        <section className="dashboard-page">
+            <title className="dashboard-title">EquiVate HR Home</title>
 
+            <div className="dashboard">Placeholder for HR dashboard
+                 <div className="industry-scales">Placeholder for Industry Scales</div>
+                 <div className="employee-data">Placeholder for Employee Data</div>
+            </div>
 
+        </section>
+    
+    )
 
 }
 
