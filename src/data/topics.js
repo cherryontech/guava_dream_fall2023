de8015d0ed0077dd11432c9cd59fbd00f70ec98a
Gabby Recny <gjtrecny@gmail.com>
2023-10-30 11:51:31 -0600

    feat: Create placeholder data for assessment questions

---
+++ src/data/topics.js
@@ -0,0 +1,110 @@
+const topicsData = [
+    {
+        topicName: "Employee Satisfaction",
+        topicId: index,
+        questions: [
+            {
+                name: "Topic 1"
+            },
+            {
+                name: "Topic 2"
+            },
+            {
+                name: "Topic 3"
+            },
+            {
+                name: "Topic 4"
+            },
+        ],
+    },
+    {
+        topicName: "Mental Health",
+        topicId: index,
+        questions: [
+            {
+                name: "Topic 1"
+            },
+            {
+                name: "Topic 2"
+            },
+            {
+                name: "Topic 3"
+            },
+            {
+                name: "Topic 4"
+            },
+        ],
+    },
+    {
+        topicName: "Employee Engagement",
+        topicId: index,
+        questions: [
+            {
+                name: "Topic 1"
+            },
+            {
+                name: "Topic 2"
+            },
+            {
+                name: "Topic 3"
+            },
+            {
+                name: "Topic 4"
+            },
+        ],
+    },
+    {
+        topicName: "Diversity, Equity and Inclusion",
+        topicId: index,
+        questions: [
+            {
+                name: "Topic 1"
+            },
+            {
+                name: "Topic 2"
+            },
+            {
+                name: "Topic 3"
+            },
+            {
+                name: "Topic 4"
+            },
+        ],
+    },
+    {
+        topicName: "Employee Experience",
+        topicId: index,
+        questions: [
+            {
+                name: "Topic 1"
+            },
+            {
+                name: "Topic 2"
+            },
+            {
+                name: "Topic 3"
+            },
+            {
+                name: "Topic 4"
+            },
+        ],
+    },
+    {
+        topicName: "Benefits",
+        topicId: index,
+        questions: [
+            {
+                name: "Topic 1"
+            },
+            {
+                name: "Topic 2"
+            },
+            {
+                name: "Topic 3"
+            },
+            {
+                name: "Topic 4"
+            },
+        ],
+    },
+]
