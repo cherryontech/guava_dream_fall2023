7540a73369d61fa7f5c366f4fd916a4c79ef0634
Gabby Recny <gjtrecny@gmail.com>
2023-10-30 14:14:43 -0600

    feature: Create basic functionality for assessment questionnaire

--- src/Components/AssessmentTopics.js
+++ src/Components/AssessmentTopics.js
@@ -1,8 +1,30 @@
 import React from "react";
 
-const AssessmentTopics = () => {
+const AssessmentTopics = ({subject, questions}) => {
+    const handleChange = (event) => {
+        console.log(event.target.value)
+    }
+    
+    const renderQuestions = questions.map((question, index) => {
+        return (
+            <div key={index}>
+                <label htmlFor="checkbox">{question.name}</label>
+                    <input
+                    type="checkbox"
+                    id="checkbox"
+                    key={index}
+                    value={question.name}
+                    defaultChecked={false}
+                    onChange={(event) => handleChange(event)}
+                />
+            </div>
+        )
+    })
     return (
-        <p>AssessmentTopics</p>
+            <article>
+                <h3>{subject}</h3>
+                {renderQuestions}
+            </article>
     )
 }
 
--- src/Components/CreateAssessment.js
+++ src/Components/CreateAssessment.js
@@ -1,7 +1,23 @@
 import React from "react";
 import AssessmentTopics from "./AssessmentTopics";
+import topicsData from "../data/topics";
 
 const CreateAssessment = () => {
+    const topicsMap = topicsData.map((topic, index) => {
+        return (
+            <AssessmentTopics 
+                subject={topic.name}
+                id={topic.id}
+                questions={topic.questions}
+                key={index}
+            />
+        )
+    })
+
+        const formSubmitHandler = (e) => {
+        e.preventDefault()
+        console.log('Form Handler', e.target.value)
+    }
     return (
         <section className="create-assessment-container">
             <div>
@@ -9,7 +25,10 @@ const CreateAssessment = () => {
                 <p>Select what you want to discover about you team</p>
             </div>
             <section className="assessment-selection-container">
-                <AssessmentTopics />
+                <form onSubmit={(e )=> formSubmitHandler(e)}>
+                    {topicsMap}
+                <button type="submit">Submit</button>
+                </form>
             </section>
         </section>
     )
