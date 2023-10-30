067728fd27df89d9f254576d2d7421f28edaa11f
Gabby Recny <gjtrecny@gmail.com>
2023-10-30 15:50:12 -0600

    style: Add 'jit' mode for faster compiling and add blue color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    colors: {
+      'blue': '#466FB4',
+    },
     extend: {},
   },
   plugins: [],
