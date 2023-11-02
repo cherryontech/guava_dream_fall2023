9a11d9a2a34a80ed716a22da0742bfa39ca5a24a
meganbyrd22 <112498787+meganbyrd22@users.noreply.github.com>
2023-11-02 09:11:01 -0500

    chore: Remove React app placeholder

--- src/App.js
+++ src/App.js
@@ -7,25 +7,6 @@ function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <Dashboard />
     </div>
