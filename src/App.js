0eb130d8fe02cdf2e65cdfa15c5c55b3b1672d13
meganbyrd22 <112498787+meganbyrd22@users.noreply.github.com>
2023-11-02 12:50:54 -0500

    chore: Remove header from app placeholder

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Dashboard from "./Pages/Dashboard/Dashboard.js";
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-      </header>
       <Dashboard />
     </div>
   );
