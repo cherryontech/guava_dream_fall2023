4bdc5bf1a14506baf0d415c175bb5c3f28409d98
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-11-02 23:25:57 -0400

    fix image not show issue

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,6 +3,7 @@ import {FaTimes} from 'react-icons/fa';
 import {BiMenu} from 'react-icons/bi';
 import { useState } from 'react';
 import "./Navbar.css";
+import Logo from "./logo.png";
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
@@ -10,44 +11,44 @@ const Navbar = () => {
         setClick(!click);
     }
     const content = <>
-        <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition tc">
+        <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition tc mt-5 bg-white">
             <ul className="text-center text-xl p-15 flex flex-col">
                 <Link spy={true} smooth={true} to="App">
-                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Dashboard</li>
+                    <li className="m-4 p-4 border-b border-violet-500 hover:bg-slate-800 hover:rounded w-9/10">Dashboard</li>
                 </Link>
                 <Link spy={true} smooth={true} to="About">
-                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">About</li>
+                    <li className="m-4 p-4 border-b border-violet-500 hover:bg-slate-800 hover:rounded w-9/10">About</li>
                 </Link>
                 <Link spy={true} smooth={true} to="Package">
-                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Resources</li>
+                    <li className="m-4 p-4 border-b border-violet-500 hover:bg-slate-800 hover:rounded w-9/10">Resources</li>
                 </Link>
                 <Link spy={true} smooth={true} to="Book">
-                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Community</li>
+                    <li className="m-4 p-4 border-b border-violet-500 hover:bg-slate-800 hover:rounded w-9/10">Community</li>
                 </Link>
             </ul>
         </div>
     </>
     return (
         <nav>
-            <div className="h-10vh flex justify-between z-50 tc lg:py-5 px-20 py-4">
+            <div className="h-10vh flex justify-between z-50 tc lg:py-5 py-4 m-3">
                 <div className="flex items-center flex-1">
-                <img src="./logo.png" />
+                <img src={ Logo } className="w-5 m-4" />
                     <span className="text-xl font-bold">EquiVate</span>
                 </div>
                 <div className="lg:flex md:flex lg: flex-1 items-center justify-end font-normal hidden">
                     <div className="flex gap-8 mr-6 text-[18px]">
                         <ul className="flex gap-6">
                             <Link spy={true} smooth={true} to="App">
-                            <li className="hover:text-fuchsia-600 transtion border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">Dashboard</li>
+                            <li className="hover:text-fuchsia-600 transtion border-b-2 border-violet-500 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">Dashboard</li>
                             </Link>
                             <Link spy={true} smooth={true} to="About">
-                                <li className="hover:text-fuchsia-600 transtion border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">About</li>
+                                <li className="hover:text-fuchsia-600 transtion border-b-2 border-violet-500 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">About</li>
                             </Link>
                             <Link spy={true} smooth={true} to="Package">
-                                <li className="hover:text-fuchsia-600 transtion border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">Resources</li>
+                                <li className="hover:text-fuchsia-600 transtion border-b-2 border-violet-500 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">Resources</li>
                             </Link>
                             <Link spy={true} smooth={true} to="Book">
-                                <li className="hover:text-fuchsia-600 transtion border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">Community</li>
+                                <li className="hover:text-fuchsia-600 transtion border-b-2 border-violet-500 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">Community</li>
                             </Link>
                         </ul>
                     </div>
