8ef0447c8037a498a56cb7ecd714587b6e90ee95
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-11-02 22:50:07 -0400

    add navbar following the figma design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './Navbar';
 import Dashboard from "./Pages/Dashboard/Dashboard.js";
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <Dashboard />
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,67 @@
-function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-        <li>Item 5</li>
-      </ul>
-    </nav>
-  );
+import { Link } from "react-scroll";
+import {FaTimes} from 'react-icons/fa';
+import {BiMenu} from 'react-icons/bi';
+import { useState } from 'react';
+import "./Navbar.css";
+
+const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => {
+        setClick(!click);
+    }
+    const content = <>
+        <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition tc">
+            <ul className="text-center text-xl p-15 flex flex-col">
+                <Link spy={true} smooth={true} to="App">
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Dashboard</li>
+                </Link>
+                <Link spy={true} smooth={true} to="About">
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">About</li>
+                </Link>
+                <Link spy={true} smooth={true} to="Package">
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Resources</li>
+                </Link>
+                <Link spy={true} smooth={true} to="Book">
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Community</li>
+                </Link>
+            </ul>
+        </div>
+    </>
+    return (
+        <nav>
+            <div className="h-10vh flex justify-between z-50 tc lg:py-5 px-20 py-4">
+                <div className="flex items-center flex-1">
+                <img src="./logo.png" />
+                    <span className="text-xl font-bold">EquiVate</span>
+                </div>
+                <div className="lg:flex md:flex lg: flex-1 items-center justify-end font-normal hidden">
+                    <div className="flex gap-8 mr-6 text-[18px]">
+                        <ul className="flex gap-6">
+                            <Link spy={true} smooth={true} to="App">
+                            <li className="hover:text-fuchsia-600 transtion border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">Dashboard</li>
+                            </Link>
+                            <Link spy={true} smooth={true} to="About">
+                                <li className="hover:text-fuchsia-600 transtion border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">About</li>
+                            </Link>
+                            <Link spy={true} smooth={true} to="Package">
+                                <li className="hover:text-fuchsia-600 transtion border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">Resources</li>
+                            </Link>
+                            <Link spy={true} smooth={true} to="Book">
+                                <li className="hover:text-fuchsia-600 transtion border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer whitespace-nowrap">Community</li>
+                            </Link>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    {click && content}
+                </div>
+
+                <button className="text-3xl  block sm:hidden transtion" onClick={handleClick}>
+                    {click ? <FaTimes/> : < BiMenu/>}
+                </button>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
