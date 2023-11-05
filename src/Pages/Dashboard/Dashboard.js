b5c66034ddf7da7ffde0411d38056a1d88797f5d
meganbyrd22 <meganbyrd22@gmail.com>
2023-11-05 13:20:33 -0600

    chore: Remove border color from boxes chore: Change page background to gray chore: Change box background to white chore: Add padding to welcome box for text

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,24 +1,19 @@
-import "./Dashboard.css"
-import Navbar from "./Navbar.js"
+import "./Dashboard.css";
 
 const Dashboard = () => {
     return (
         <div className="page">
-        <Navbar />
-        //Add SideMenu component
-        <section className="dashboard-container">
-            <h1 className="welcome-box">Hello</h1>
-            <div className="employee-satisfaction-box">Employee Satisfaction Data</div>
-            <div className="mental-health-box">Mental health Data</div>
-            <div className="employee-engagement-box">Employee Enagement Data</div>
-            <div className="dei-box">DEI Data</div>
-            <div className="employee-experience-box">Employee Experience Data</div>
-            <div className="benefits-box">Benefits Data</div>
-        </section>
+            <section className="dashboard-container">
+                <h1 className="welcome-box">Hello!</h1>
+                <div className="employee-satisfaction-box">Employee Satisfaction Data</div>
+                <div className="mental-health-box">Mental health Data</div>
+                <div className="employee-engagement-box">Employee Enagement Data</div>
+                <div className="dei-box">DEI Data</div>
+                <div className="employee-experience-box">Employee Experience Data</div>
+                <div className="benefits-box">Benefits Data</div>
+            </section>
         </div>
-    
     )
-
 }
 
 export default Dashboard
