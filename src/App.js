a5630188f8cb1eac9efc78cba342c488963398a9
Gabby Recny <gjtrecny@gmail.com>
2023-11-06 16:16:04 -0600

    feat: Create functionality to topic data to new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import CreateAssessment from './Components/CreateAssessment';
+import ChooseQuestions from './Components/ChooseQuestions';
+import { useState } from 'react';
 
 function App() {
+  const [ selectedFields, setSelectedFields ] = useState([])
+  const displayQuestions = (questions) => {
+    setSelectedFields(questions)
+  }
   return (
     <div className="bg-background-blue flex items-center justify-center h-screen">
-      <CreateAssessment/>
+      <CreateAssessment displayQuestions={displayQuestions}/>
+      <ChooseQuestions topics={selectedFields}/>
     </div>
   );
 }
