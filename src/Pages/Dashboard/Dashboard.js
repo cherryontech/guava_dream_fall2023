444ec64b8405b93c6cb53a3eb7f4ae31372ec18f
meganbyrd22 <meganbyrd22@gmail.com>
2023-11-06 14:56:27 -0600

    chore: Change display of dashboard from grid to flex

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,13 +4,17 @@ const Dashboard = () => {
     return (
         <div className="page">
             <section className="dashboard-container">
-                <h1 className="welcome-box">Hello!</h1>
-                <div className="employee-satisfaction-box">Employee Satisfaction Data</div>
+                <div className="row1">
+                    <h1 className="welcome-box">Hello!</h1>
+                    <div className="employee-satisfaction-box">Employee Satisfaction Data</div>
+                </div>
                 <div className="mental-health-box">Mental health Data</div>
-                <div className="employee-engagement-box">Employee Enagement Data</div>
-                <div className="dei-box">DEI Data</div>
-                <div className="employee-experience-box">Employee Experience Data</div>
-                <div className="benefits-box">Benefits Data</div>
+                <div className="squareOfBoxes">
+                    <div className="employee-engagement-box">Employee Enagement Data</div>
+                    <div className="dei-box">DEI Data</div>
+                    <div className="employee-experience-box">Employee Experience Data</div>
+                    <div className="benefits-box">Benefits Data</div>
+                </div>
             </section>
         </div>
     )
