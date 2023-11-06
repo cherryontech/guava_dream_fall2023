e11bfc911a0dd530537222b602cfb2fced7a6c0c
Gabby Recny <gjtrecny@gmail.com>
2023-11-06 15:00:58 -0600

    chore: Rename keys for better readability

--- src/data/topics.js
+++ src/data/topics.js
@@ -1,109 +1,109 @@
 const topicsData = [
     {
         name: "Employee Satisfaction",
-        id: Date.now(),
-        questions: [
+        id: "employee-satisfaction",
+        subTopics: [
             {
-                name: "Topic 1"
+                name: "Topic 5"
             },
             {
-                name: "Topic 2"
+                name: "Topic 6"
             },
             {
-                name: "Topic 3"
+                name: "Topic 7"
             },
             {
-                name: "Topic 4"
+                name: "Topic 8"
             },
         ],
     },
     {
         name: "Mental Health",
-        id: Date.now(),
-        questions: [
+        id: "mental-health",
+        subTopics: [
             {
-                name: "Topic 1"
+                name: "Topic 9"
             },
             {
-                name: "Topic 2"
+                name: "Topic 10"
             },
             {
-                name: "Topic 3"
+                name: "Topic 11"
             },
             {
-                name: "Topic 4"
+                name: "Topic 12"
             },
         ],
     },
     {
         name: "Employee Engagement",
-        id: Date.now(),
-        questions: [
+        id: "employee-engagement",
+        subTopics: [
             {
-                name: "Topic 1"
+                name: "Topic 13"
             },
             {
-                name: "Topic 2"
+                name: "Topic 14"
             },
             {
-                name: "Topic 3"
+                name: "Topic 15"
             },
             {
-                name: "Topic 4"
+                name: "Topic 16"
             },
         ],
     },
     {
         name: "Diversity, Equity and Inclusion",
-        id: Date.now(),
-        questions: [
+        id: 'dei',
+        subTopics: [
             {
-                name: "Topic 1"
+                name: "Topic 17"
             },
             {
-                name: "Topic 2"
+                name: "Topic 18"
             },
             {
-                name: "Topic 3"
+                name: "Topic 19"
             },
             {
-                name: "Topic 4"
+                name: "Topic 20"
             },
         ],
     },
     {
         name: "Employee Experience",
-        id: Date.now(),
-        questions: [
+        id: "employee-experience",
+        subTopics: [
             {
-                name: "Topic 1"
+                name: "Topic 21"
             },
             {
-                name: "Topic 2"
+                name: "Topic 22"
             },
             {
-                name: "Topic 3"
+                name: "Topic 23"
             },
             {
-                name: "Topic 4"
+                name: "Topic 24"
             },
         ],
     },
     {
         name: "Benefits",
-        id: Date.now(),
-        questions: [
+        id: "benefits",
+        subTopics: [
             {
-                name: "Topic 1"
+                name: "Topic 25"
             },
             {
-                name: "Topic 2"
+                name: "Topic 25"
             },
             {
-                name: "Topic 3"
+                name: "Topic 27"
             },
             {
-                name: "Topic 4"
+                name: "Topic 28"
             },
         ],
     },
