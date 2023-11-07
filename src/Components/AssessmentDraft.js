803903b517acc2de3f6b8e03876e95c9afd51592
Gabby Recny <gjtrecny@gmail.com>
2023-11-07 16:38:32 -0600

    feature: Create basic functionality to view selected assessment questions

---
+++ src/Components/AssessmentDraft.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Slider } from '@mui/material';
+
+const AssessmentDraft = ({ assessmentQuestions }) => {
+  const marks = [
+    {
+      value: 0,
+      label: '0',
+    },
+    {
+      value: 1,
+      label: '1',
+    },
+    {
+      value: 2,
+      label: '2',
+    },
+    {
+      value: 3,
+      label: '3',
+    },
+    {
+      value: 4,
+      label: '4',
+    },
+    {
+      value: 5,
+      label: '5',
+    },
+  ];
+
+  const valuetext = (value) => {
+    return `${value}`;
+  };
+
+  const displayAssessmentQuestions = assessmentQuestions.map((question, index) => {
+    return (
+      <div key={index}>
+        <h3>{question}</h3>
+        <Slider
+          aria-label="Always visible"
+          defaultValue={3}
+          getAriaValueText={valuetext}
+          step={null}
+          valueLabelDisplay="on"
+          marks={marks}
+        />
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <p>AssessmentDraft</p>
+      {console.log('Assessment Questions', assessmentQuestions)}
+      {displayAssessmentQuestions}
+    </>
+  );
+};
+
+export default AssessmentDraft;
