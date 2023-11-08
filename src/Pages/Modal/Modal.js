5541125df173ed05eb129d2e1c30ba3c8cb21bab
Gabby Recny <gjtrecny@gmail.com>
2023-11-08 15:49:37 -0600

    chore: Adjust indents to resolve esLint

--- src/Pages/Modal/Modal.js
+++ src/Pages/Modal/Modal.js
@@ -3,24 +3,25 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Modal = ({ setIsOpen }) => {
-    return (
-    <section>
-      <div className=".darkBG" onClick={() => setIsOpen(false)} />
+  return (
+  <section>
+    <div className=".darkBG" onClick={() => setIsOpen(false)} />
       <div className=".centered">
         <div className=".modal">
           <div className=".modalHeader">
-            <h5 className=".heading">Would you like to continue to accessment page or add more questions?</h5>
+            <h5 className=".heading">
+              Would you like to continue to accessment page or add more questions?
+            </h5>
           </div>
-            <button className=".closeBtn" onClick={() => setIsOpen(false)}>
+          <button className=".closeBtn" onClick={() => setIsOpen(false)}>
             Add more questions
           </button>
           <Link to={"assessment-draft"}>
             <button onClick={() => setIsOpen(false)}>Continue to assessment page</button>
           </Link>
-          {/* // ... */}
         </div>
       </div>
-    </section>
+  </section>
 )
 };
 
