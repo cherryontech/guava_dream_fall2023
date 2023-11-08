e2de222fc45de55fa17cbc043c12c933ba9b6132
Gabby Recny <gjtrecny@gmail.com>
2023-11-08 13:43:00 -0600

    chore: Update App with assessment components and functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import './Firebase.js';
 import Dashboard from "./Pages/Dashboard/Dashboard.js";
 import About from "./About";
 import Community from "./Community";
+import ViewAssessmentDraft from './Pages/ViewAssessmentDraft.js';
+import CreateAssessment from './Pages/CreateAssessment.js';
+import ChooseQuestions from './Pages/ChooseQuestions.js';
+
 function App() {
+
+  const [selectedFields, setSelectedFields] = useState([]);
+  const [assessmentQuestions, setAssessmentQuestions] = useState([]);
+
+  const displayQuestions = (questions) => {
+    setSelectedFields(questions);
+  };
+
+  const addQuestionsToAssessment = (newQuestion) => {
+    const addedQuestions = [...assessmentQuestions, newQuestion];
+
+    if (!assessmentQuestions.includes(newQuestion)) {
+      setAssessmentQuestions(addedQuestions);
+    }
+  };
+
+  const deleteQuestion = (id) => {
+    const filteredQuestions = assessmentQuestions.filter((question) => question.id !== id);
+    setAssessmentQuestions(filteredQuestions);
+  };
   return (
     <BrowserRouter>
     <Navbar />
@@ -13,6 +38,16 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/about" element={<About />} />
           <Route path="/community" element={<Community />} />
+          <Route path="create-assessment" element={<CreateAssessment displayQuestions={displayQuestions}/>} />
+          {/* eslint-disable-next-line max-len */ }
+          <Route path="choose-assessment-questions" element={
+            <ChooseQuestions 
+              selectedFields={selectedFields}
+              addQuestionsToAssessment={addQuestionsToAssessment} 
+            />} 
+          />
+          <Route path="assessment-draft" element={<ViewAssessmentDraft assessmentQuestions={assessmentQuestions} deleteQuestion={deleteQuestion} />
+          }/>
     </Routes>
     </BrowserRouter>
   );
