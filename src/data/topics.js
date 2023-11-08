3b8713b1cbf2f3d46756aac23d8f9455b67a0554
Gabby Recny <gjtrecny@gmail.com>
2023-11-08 13:41:30 -0600

    chore: Reinstall data folder for mock data in assessment functionality

---
+++ src/data/topics.js
@@ -0,0 +1,467 @@
+const topicsData = [
+  {
+    name: 'Employee Satisfaction',
+    id: 'employee-satisfaction',
+    subTopics: [
+      {
+        name: 'Topic 5',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-satisfaction-t5-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-satisfaction-t5-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-satisfaction-t5-question-3',
+          },
+        ],
+      },
+      {
+        name: 'Topic 6',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-satisfaction-t6-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-satisfaction-t6-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-satisfaction-t6-question-3',
+          },
+        ],
+      },
+      {
+        name: 'Topic 7',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-satisfaction-t7-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-satisfaction-t7-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-satisfaction-t7-question-3',
+          },
+        ],
+      },
+      {
+        name: 'Topic 8',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-satisfaction-t8-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-satisfaction-t8-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-satisfaction-t8-question-3',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Mental Health',
+    id: 'mental-health',
+    subTopics: [
+      {
+        name: 'Topic 9',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'mental-health-t9-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'mental-health-t9-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'mental-health-t9-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 10',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'mental-health-t10-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'mental-health-t10-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'mental-health-t10-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 11',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'mental-health-t11-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'mental-health-t11-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'mental-health-t11-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 12',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'mental-health-t12-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'mental-health-t12-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'mental-health-t12-question-3',
+          },
+        ],
+
+      },
+    ],
+  },
+  {
+    name: 'Employee Engagement',
+    id: 'employee-engagement',
+    subTopics: [
+      {
+        name: 'Topic 13',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-engagement-t13-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-engagement-t13-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-engagement-t13-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 14',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-engagement-t14-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-engagement-t14-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-engagement-t14-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 15',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-engagement-t15-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-engagement-t15-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-engagement-t15-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 16',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-engagement-t16-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-engagement-t16-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-engagement-t16-question-3',
+          },
+        ],
+
+      },
+    ],
+  },
+  {
+    name: 'Diversity, Equity and Inclusion',
+    id: 'dei',
+    subTopics: [
+      {
+        name: 'Topic 17',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'dei-t17-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'dei-t17-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'dei-t17-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 18',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'dei-t18-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'dei-t18-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'dei-t18-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 19',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'dei-t19-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'dei-t19-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'dei-t19-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 20',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'dei-t20-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'dei-t20-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'dei-t20-question-3',
+          },
+        ],
+
+      },
+    ],
+  },
+  {
+    name: 'Employee Experience',
+    id: 'employee-experience',
+    subTopics: [
+      {
+        name: 'Topic 21',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-experience-t21-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-experience-t21-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-experience-t21-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 22',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-experience-t22-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-experience-t22-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-experience-t22-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 23',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-experience-t23-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-experience-t23-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-experience-t23-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 24',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'employee-experience-t24-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'employee-experience-t24-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'employee-experience-t24-question-3',
+          },
+        ],
+
+      },
+    ],
+  },
+  {
+    name: 'Benefits',
+    id: 'benefits',
+    subTopics: [
+      {
+        name: 'Topic 25',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'benefits-t25-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'benefits-t25-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'benefits-t25-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 26',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'benefits-t26-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'benefits-t26-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'benefits-t26-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 27',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'benefits-t27-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'benefits-t27-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'benefits-t27-question-3',
+          },
+        ],
+
+      },
+      {
+        name: 'Topic 28',
+        inquiries: [
+          {
+            name: 'Question-1',
+            id: 'benefits-t28-question-1',
+          },
+          {
+            name: 'Question-2',
+            id: 'benefits-t28-question-2',
+          },
+          {
+            name: 'Question-3',
+            id: 'benefits-t28-question-3',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default topicsData;
