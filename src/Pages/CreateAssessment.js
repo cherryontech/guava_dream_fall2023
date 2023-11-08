a396aaae88abad230fb96f31a5ae8fe67e154e54
Gabby Recny <gjtrecny@gmail.com>
2023-11-08 13:32:56 -0600

    debug: Re-create assessment files to resolve merge conflict

---
+++ src/Pages/CreateAssessment.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import AssessmentTopics from './AssessmentTopics';
+import topicsData from '../data/topics';
+
+const CreateAssessment = ({ displayQuestions }) => {
+  const [selectedTopics, setSelectedTopics] = useState([]);
+
+  const addSelectedTopics = (topic) => {
+    const newTopics = [...selectedTopics, topic];
+    setSelectedTopics(newTopics);
+  };
+
+  const topicsMap = topicsData.map((topic) => (
+    <AssessmentTopics
+      subject={topic.name}
+      id={topic.id}
+      subTopics={topic.subTopics}
+      key={topic.id}
+      addSelectedTopics={addSelectedTopics}
+    />
+  ));
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    displayQuestions(selectedTopics);
+  };
+
+  return (
+    <section>
+      <div className="bg-white w-2/3 rounded-xl py-3.5 px-3 m-4">
+        <h3 className="text-blue">Create Assessment</h3>
+        <p>Select what you want to discover about you team</p>
+      </div>
+      <form
+        className="grid grid-cols-3 gap-4 h-full m-4"
+        onSubmit={(e) => formSubmitHandler(e)}
+      >
+        {topicsMap}
+        <button
+          type="submit"
+          className="bg-white text-blue rounded-md shadow-lg absolute top-0 right-0 h-16 w-16"
+        >
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+};
+
+CreateAssessment.propTypes = {
+  displayQuestions: PropTypes.func.isRequired,
+};
+
+export default CreateAssessment;
+
