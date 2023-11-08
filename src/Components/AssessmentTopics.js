c5229dbca92ff5f320f742aaa8b79b39e0ce3129
Gabby Recny <gjtrecny@gmail.com>
2023-11-07 19:17:25 -0600

    chore: Adjust spacing and add typing to resolve esLint errors

--- src/Components/AssessmentTopics.js
+++ src/Components/AssessmentTopics.js
@@ -1,42 +1,46 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const AssessmentTopics = ({ subject, subTopics, addSelectedTopics}) => {
+const AssessmentTopics = ({ subject, subTopics, addSelectedTopics }) => {
   const handleChange = (subTopic) => {
     const selectedTopic = {
       name: subTopic.name,
-      subject: subject,
+      subject: { subject },
       questions: subTopic.inquiries,
     };
     addSelectedTopics(selectedTopic);
   };
 
-
-
-    const renderSubTopics = subTopics.map((subTopic, index) => {
-        return (
-        <div key={index} 
-        className="bg-white text-blue border-solid border-blue border-2 rounded-lg hover:bg-blue hover:cursor-pointer hover:text-white w-2/3">
-            <label htmlFor="checkbox">
-                {subTopic.name}
-            </label>
-            <input
-                type="checkbox"
-                id="checkbox"
-                key={index}
-                value={subTopic}
-                defaultChecked={false}
-                onChange={() => handleChange(subTopic)}
-                // className="opacity-0"
-            />
-        </div>
-        )
-    })
-    return (
+  const renderSubTopics = subTopics.map((subTopic) => (
+    <div
+      key={Date.now()}
+      className="bg-white text-blue border-solid border-blue border-2 rounded-lg hover:bg-bluehover:cursor-pointer hover:text-white w-2/3"
+    >
+      <label htmlFor="checkbox">
+        {subTopic.name}
+      </label>
+      <input
+        type="checkbox"
+        id="checkbox"
+        value={subTopic}
+        defaultChecked={false}
+        onChange={() => handleChange(subTopic)}
+        // className="opacity-0"
+      />
+    </div>
+  ));
+  return (
     <article className="p-3.5 text-center flex flex-col justify-evenly items-center bg-white rounded-xl">
-        <h3 className="my-1 font-semibold">{subject}</h3>
-            {renderSubTopics}
+      <h3 className="my-1 font-semibold">{subject}</h3>
+      {renderSubTopics}
     </article>
-    )
-}
+  );
+};
+
+AssessmentTopics.propTypes = {
+  subject: PropTypes.string.isRequired,
+  subTopics: PropTypes.arrayOf(PropTypes.shape).isRequired,
+  addSelectedTopics: PropTypes.func.isRequired,
+};
 
 export default AssessmentTopics;
