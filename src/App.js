af3268c630f58e493d25a57070a3100585653c62
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-11-08 22:18:11 -0500

    add input file for entering name and email

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './Firebase.js';
 import Dashboard from "./Pages/Dashboard/Dashboard.js";
 import About from "./About";
 import Community from "./Community";
+import Input from "./Input";
 function App() {
   return (
     <BrowserRouter>
     <Navbar />
+    <Input />
     <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/about" element={<About />} />
