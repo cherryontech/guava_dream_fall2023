1f38f2c39aecd4a6baecb458f61745e9929d1630
meganbyrd22 <meganbyrd22@gmail.com>
2023-11-10 15:49:45 -0600

    chore: Style login page per figma

--- src/Login.js
+++ src/Login.js
@@ -30,57 +30,60 @@ const Login = () => {
  
     return(
         <>
-            <div className="h-screen bg-gradient-to-l from-indigo-300 flex justify-center items-center ">
-            <main >        
-                <section className="flex justify-center" >
-                    <div className="flex flex-col w-96 ">
-                        <h1 className="text-4xl font-bold text-indigo-400 font-dm-sans">{`Let's start listening to your employees`}</h1>
+            <div className="h-screen bg-gradient-to-l from-indigo-300 to-60% flex justify-center items-center font-dm-sans">
+            <main className="mt-0" >        
+                <section className="flex justify-center gap-x-36 mt-0" >
+                    <div className="flex flex-col w-96 gap-y-6" >
+                        <h1 className="text-4xl font-bold text-indigo-400">{`Let's start listening to your employees`}</h1>
                         <p className="text-sm text-align-left text-slate-500">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit nullam neque ultrices.</p>
-                        <img src= { Image } alt="Three cartoon people sitting in front of a computer with lightbulbs above their heads"></img>
+                    <div className="mt-24">
+                        <img src={ Image } alt="Three cartoon people sitting in front of a computer with lightbulbs above their heads"></img>
                     </div>
-                    <div className="flex flex-col border-4 bg-white rounded-xl m-8 p-3 space-y-4 w-96 h-80" >                                            
-                        <p className="text-xl font-bold text-indigo-400"> Login to your account </p>                                                     
-                        <form className="space-y-6">                                              
+                    </div>
+
+                    <div className="flex flex-col border-4 border-none bg-white rounded-xl m-8  pl-1 pr-8 pt-10 space-y-4 w-2/4 h-96 align-center" >                                            
+                        <p className="text-2xl font-semi-bold text-slate-500 mt-4 pl-36"> Login to your account </p>                                                     
+                        <form className="space-y-6 flex flex-col pl-16 pr-0">                                              
                             <div className="flex flex-col">
-                                <label htmlFor="email-address">
-                                    Email address
+                                <label htmlFor="email-address" className="text-xs font-light mb-1">
+                                    Work Email
                                 </label>
-                                <input className="border-2 rounded-lg space-x-2 text-sm"
+                                <input className="border-2 rounded-lg space-x-2 text-sm p-2 w-96"
                                     id="email-address"
                                     name="email"
                                     type="email"                                    
                                     required                                                                                
-                                    placeholder="Email address"
+                                    placeholder= "Enter your email"
                                     onChange={(e)=>setEmail(e.target.value)}
                                 />
                             </div>
 
                             <div className="flex flex-col">
-                                <label htmlFor="password">
+                                <label htmlFor="password" className="text-xs font-light mb-1">
                                     Password
                                 </label>
-                                <input className="border-2 rounded-lg space-x-2 text-sm"
+                                <input className="border-2 rounded-lg space-x-2 text-sm p-2 w-96"
                                     id="password"
                                     name="password"
                                     type="password"                                    
                                     required                                                                                
-                                    placeholder="Password"
+                                    placeholder="Enter your password"
                                     onChange={(e)=>setPassword(e.target.value)}
                                 />
                             </div>
                                                
                             <div>
-                                <button className="border-2 rounded-lg bg-indigo-400 text-white font-bold p-1.5"                                   
+                                <button className="border-2 rounded-lg bg-indigo-400 text-white font-bold p-1.5 w-96 font-thin text-xs p-3"                                   
                                     onClick={onLogin}                                        
                                 >      
-                                    Login                                                                  
+                                    Login now                                                                 
                                 </button>
                             </div> 
                                   
                         </form>
                        
-                        <p className="text-xs text-black text-center pt-6">
-                            No account yet? {' '}
+                        <p className="text-xs text-stone-300 text-center pl-6 pt-0">
+                             {`Don't Have An Account?`}{' '}
                             <NavLink to="/signup" className={"hover:text-indigo-400"}>
                                 Sign up
                             </NavLink>
