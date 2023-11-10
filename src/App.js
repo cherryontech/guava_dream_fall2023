2af52ec16da8b1b0cdeaad5b42c7c9c1e0cbf249
meganbyrd22 <meganbyrd22@gmail.com>
2023-11-10 12:02:09 -0600

    fix: Remove input component from App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ import About from "./About";
 import Community from "./Community";
 import Login from "./Login.js";
 import Signup from './Signup.js';
-import Input from "./Input";
+
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar />
-    <Input />
     <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/about" element={<About />} />
