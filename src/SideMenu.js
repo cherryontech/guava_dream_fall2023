eec35468f023d0cc0dc0d0f06b63b548e8c40e9f
meganbyrd22 <meganbyrd22@gmail.com>
2023-11-29 13:57:14 -0600

    fix: Edit routes in SideMenu to correct pages

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -23,7 +23,7 @@ const SideMenu = () => {
     const handleLogout = () => {               
         signOut(auth).then(() => {
         // Sign-out successful.
-            navigate("/");
+            navigate("/Home");
             console.log("Signed out successfully")
         }).catch((error) => {
             console.error("Error occured during sign-out:", error);
@@ -33,16 +33,16 @@ const SideMenu = () => {
     const content = <>
         <div className="lg:hidden block absolute inset-y-0  top-0 h-full left-0 bg-slate-900 transition tc mt-5 bg-white">
             <ul className="text-center text-xl p-15 flex flex-col">
-            <NavLink to="./Pages/Dashboard">
+            <NavLink to="dashboard">
                 Dashboard
             </NavLink>
-            <NavLink to="./Pages/CreateAssessment">
+            <NavLink to="assessments">
                 Assessments
             </NavLink>
-            <NavLink to="./Pages/CreateAssessment">
+            <NavLink to="/create-assessment">
                 Create Assessment
             </NavLink>
-            <NavLink to="/Resources">
+            <NavLink to="/resources">
                 Resources
             </NavLink>
             </ul>
@@ -52,32 +52,36 @@ const SideMenu = () => {
         
         <sidebar>
             <div className="flex flex-col justify-between tc m-6 inset-y-0 max-w-max">
+                <NavLink to="/Home">
                 <div className="flex flex-col justify-self-center flex-1 mb-2">
                 <img src={ LogoIcon } className="ml-20 w-12 m-12" alt="EquiVate logo, pen inside of box" />
                 </div>
+                </NavLink>
                 <div className=" md:flex lg: flex-1 items-center font-normal hidden">
                     <div className="flex gap-8 mr-6 text-[18px]">
                         <ul className="flex flex-col gap-2 mt-1">
                         <button className="flex justify-start self-center items-center text-white rounded-full bg-indigo-400 w-48 h-16 p-2 font-bold">
                             <img className="w-8 h-8 mr-4"src={ DashboardIcon } alt="icon square with four circles inside" />
-                        <NavLink to="./Pages/Dashboard" className="flex">
+                        <NavLink to="/dashboard" className="flex">
                             Dashboard
                         </NavLink>
                         </button>
-                        <NavLink to="./Pages/Assessments"className='flex p-2 space-x-6 items-center  text-slate-500'>
+                        <NavLink to="/assessments"className='flex p-2 space-x-6 items-center  text-slate-500'>
                                 <img src={ AssessmentsIcon } alt="Icon of piece of paper with upward facing arrow" className='w-8 h-8 mr-2'/>
                                 Assessments
                         </NavLink>
-                        <NavLink to="./Pages/CreateAssessment"className='flex p-2 space-x-6 items-center  text-slate-500'>
+                        <NavLink to="/create-assessment"className='flex p-2 space-x-6 items-center  text-slate-500'>
                                 <img src={ CreateAssessmentIcon } alt="Icon of piece of paper with upward facing arrow" className='w-8 h-8 mr-2'/>
                                 Create Assessment
                         </NavLink>
-                        <NavLink to="./Resources" className='flex p-2 space-x-6 items-center text-slate-500 '>
+                        <NavLink to="/resources" className='flex p-2 space-x-6 items-center text-slate-500 '>
                             <img src={ ResourcesIcon } alt="Icon of lightbulb" className='w-8 h-8 mr-2' />
                             Resources
                         </NavLink>
                         <button className="text-indigo-400 rounded-full border-2 border-indigo-400 bg-white w-24 h-10 text-xs mt-6 ml-12" onClick={handleLogout}> 
+                        <NavLink to="/community"> 
                             Logout
+                        </NavLink>   
                         </button>
                         </ul>
                     </div>
