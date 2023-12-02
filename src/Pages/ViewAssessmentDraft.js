fd8a5cc9bd463c56b6cba1408cb76c7aceb22d9b
meganbyrd22 <meganbyrd22@gmail.com>
2023-12-02 17:02:00 -0600

    chore: Add style to ViewAssessmentDraft

--- src/Pages/ViewAssessmentDraft.js
+++ src/Pages/ViewAssessmentDraft.js
@@ -2,7 +2,8 @@ import PropTypes from 'prop-types';
 import { Slider } from '@mui/material';
 import { useEffect, useState } from "react";
 import emailjs from "@emailjs/browser";
-import Image from "../Assets/Title Image.png"
+import Image from "../Assets/Title Image.png";
+import SideMenu from "../SideMenu"
 
 
 const ViewAssessmentDraft = ({ assessmentQuestions, deleteQuestion }) => {
@@ -36,16 +37,21 @@ const ViewAssessmentDraft = ({ assessmentQuestions, deleteQuestion }) => {
   const valuetext = (value) => `${value}`;
 
   const displayAssessmentQuestions = assessmentQuestions.map((question, index) => (
-    <div key={index + 1}>
+    <div className="flex flex-col mb-12"key={index + 1}>
       <h3 className='font-bold text-lg ml-10'>{question.name}</h3>
       <h4 className='text-base ml-10'>{index + 1}/{assessmentQuestions.length + 1}</h4>
-      <Slider className='ml-10'
+      
+      <Slider className='m-10 w-1/4'
         aria-label="Always visible"
         defaultValue={3}
         getAriaValueText={valuetext}
         step={null}
         valueLabelDisplay="on"
         marks={marks}
+        sx={{
+          width: "500px",
+          margin: "36px", 
+        }}
       />
       <button className="mt-2 ml-8 rounded-full bg-white w-24 h-10 text-xs" type="button" onClick={() => deleteQuestion(question.id)}>Delete Question</button>
     </div>
@@ -80,8 +86,11 @@ const ViewAssessmentDraft = ({ assessmentQuestions, deleteQuestion }) => {
 
   return (
     <>
-    <main className='h-screen bg-indigo-100'>
-      <div className='pt-8'>
+    <main className='flex'>
+      <SideMenu />
+      <div className="flex flex-col bg-indigo-100 w-full">
+      <div className='flex flex-col bg-indigo-100'>
+      <div className='flex pt-8'>
       <header className='w-96 h-32 bg-white rounded-xl ml-8 p-2'>
         <div className='flex font-bold text-xl text-lightBlue-700'>
           <img src={ Image } alt="Icon of the shadow of two people"></img>
@@ -95,10 +104,11 @@ const ViewAssessmentDraft = ({ assessmentQuestions, deleteQuestion }) => {
       <p className='font-bold  text-xl m-10'>Questions Added</p>
       
       {displayAssessmentQuestions}
+      </div>
 
       <aside></aside>
       <div className=' flex flex-col ml-10 pb-12'>
-      <form className=' flex flex-col ml-2 mt-12' onSubmit={handleSubmit}>
+      <form className=' flex flex-col ml-2 mt-12 ' onSubmit={handleSubmit}>
           <h5 className='font-bold italic text-lg'> Who would you like to send the assessment to?</h5>
           <label className="mt-4" htmlFor="name" aria-label="name">Recipient Name: </label>
           <input className="border-2 rounded-lg text-sm p-2 w-60 mr-12"
@@ -116,6 +126,7 @@ const ViewAssessmentDraft = ({ assessmentQuestions, deleteQuestion }) => {
         />
           <button className="mt-8 text-white rounded-full bg-indigo-400 w-36 h-14 text-s"  type="submit" disabled={loading} >Publish assessment</button>
       </form>
+      </div>
       </div>
     </main>
       
