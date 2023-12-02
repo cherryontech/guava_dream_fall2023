84c11814636c4c9f352369b5857a736a79da4741
meganbyrd22 <meganbyrd22@gmail.com>
2023-12-02 16:09:47 -0600

    chore: Add style to Modal

--- src/Pages/Modal/Modal.js
+++ src/Pages/Modal/Modal.js
@@ -4,23 +4,21 @@ import { Link } from 'react-router-dom';
 
 const Modal = ({ setIsOpen }) => {
   return (
-  <section>
-    <div className=".darkBG" onClick={() => setIsOpen(false)} />
-      <div className=".centered">
-        <div className=".modal">
-          <div className=".modalHeader">
-            <h5 className=".heading">
+  <section className='relative top-2 mx-auto p-5 flex flex-col justify-items-center self-center items-center h-28  max-w-fit w-2/5 bg-white border rounded-full p-4 '>
+    <div onClick={() => setIsOpen(false)} />
+      
+            <h5 className="text-lg text-left font-bold pb-2">
               Would you like to continue to accessment page or add more questions?
             </h5>
+          <div className='flex justify-start gap-10'>
+            <button className="text-base text-indigo-400" onClick={() => setIsOpen(false)}>
+              Add more questions
+            </button>
+            <Link to={"assessment-draft"}>
+              <button className="text-base text-indigo-400" onClick={() => setIsOpen(false)}>Continue to assessment page</button>
+            </Link>
           </div>
-          <button className=".closeBtn" onClick={() => setIsOpen(false)}>
-            Add more questions
-          </button>
-          <Link to={"assessment-draft"}>
-            <button onClick={() => setIsOpen(false)}>Continue to assessment page</button>
-          </Link>
-        </div>
-      </div>
+  
   </section>
 )
 };
