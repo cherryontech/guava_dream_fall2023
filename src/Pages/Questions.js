1d72828452c9e7fad246f15c3deaeff1a839f209
meganbyrd22 <meganbyrd22@gmail.com>
2023-12-04 12:07:02 -0600

    chore: Add plus symbol for selecting question in Questions

--- src/Pages/Questions.js
+++ src/Pages/Questions.js
@@ -5,7 +5,11 @@ const Questions = ({ questions, targetedTopic, addQuestionsToAssessment }) => {
     <article className="p-3 text-left flex flex-col gap-6 bg-white rounded-xl w-64 h-72" key={`${question.name}-${index}`}>
       <div className='flex gap-24'>
         <h3 className="my-1 text-lg font-bold">{question.name}</h3>
-        <button className="flex justify-center items-center border border-indigo-400 rounded-full w-6 h-6 text-indigo-400  text-2xl mt-2 " type="button" onClick={() => addQuestionsToAssessment(question)}>+</button>
+        <button 
+        className="flex justify-center items-center border border-indigo-400 rounded-full w-7 h-7 text-indigo-400 text-2xl mt-1 pb-1.5 " type="button" onClick={() => addQuestionsToAssessment(question)}
+        >
+          &#43;
+        </button>
       </div>
       <p>Brief description here</p>
       <p className='border rounded-full w-24 border-black font-bold text-sm p-2 text-center'>{targetedTopic}</p>
