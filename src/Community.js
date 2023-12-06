b23b6de6e40fb95692c91b0002ff1c4fd11776cf
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-12-05 23:26:47 -0500

    create about equivate page

--- src/Community.js
+++ src/Community.js
@@ -3,10 +3,23 @@ import "./Pages/Dashboard/Dashboard.css"
 
 const Community = () => {
     return (
-        <div>
-            <section className="dashboard-page">
-                <h1 className="dashboard-title">Hello! Welcome to EquiVate HR COMMUNITY Page</h1>
-            </section>
+        <div className="bg-indigo-200 w-full">
+            <div className="bg-white w-9/12 h-72 default-modal">
+                <h2 className="text-indigo-700 font-bold">What is Equivate?</h2>
+                <p className="text-blue-600	font-normal	">Lorem ipsum dolor sit amet consectetur 
+                adipiscing eli mattis sit phasellus mollis
+                sit aliquam sit nullam neque ultrices.
+                </p>
+                <p className="text-blue-700	font-normal	">Lorem ipsum dolor sit amet consectetur 
+                adipiscing eli mattis sit phasellus mollis
+                sit aliquam sit nullam neque ultrices.
+                </p>
+                <p className="text-blue-700	font-normal	">Lorem ipsum dolor sit amet consectetur 
+                adipiscing eli mattis sit phasellus mollis
+                sit aliquam sit nullam neque ultrices.
+                </p>
+                <button className="bg-blue-600 w-20 h-12 rounded-lg text-white">Next</button>
+            </div>
         </div>
     
     )
