3c46ce8f07c61877737fbff30ecfcd27d243188f
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-12-07 22:21:07 -0500

    delete inline styles in question page

--- src/Question.js
+++ src/Question.js
@@ -15,14 +15,14 @@ const Question = () => {
                 <p className="text-blue-700	font-normal	mx-12 my-6 text-lg">Today’s Date
                 </p>
             </div>
-            <div className='flex flex-col' style={{alignItems:'center'}}>
+            <div className='flex flex-col items-center'>
                 <div className="bg-white w-9/12 h-72 mx-auto p-2 border shadow-lg rounded-lg bg-white">
                     <h2 className="text-indigo-700 font-bold text-2xl m-12">Question/ Statement</h2>
                     <p className="text-blue-600	font-normal	mx-24 my-12 text-lg">Please rate your feelings of agreement
                     from 1 - 5 about the above question/ statement. 1 meaning you completely disagree and 5 meaning you completely agree
                     </p>
                 </div>
-                <button style={{marginTop:20}} className="bg-blue-600 w-60 h-12 rounded-full text-white flex items-center float-right justify-center"
+                <button className="mt-5 bg-blue-600 w-60 h-12 rounded-full text-white flex items-center float-right justify-center"
                     onClick={handleClick}>Continue to Next Question
                 </button>
             </div>
